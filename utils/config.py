7568bc523da2fcf1616cdedf3b3d4c3b4deb3f12
janfiszer <jf.janfiszer@gmail.com>
2023-05-28 23:52:12 +0200

    yet another model training

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,8 @@
 # image and mask parameters
+RGB_CHANNELS = (3, )
 ORIGINAL_IMAGE_SIZE = (256, 256)
-IMAGE_SIZE = (32, 32)  # a param to reconsider resolution so different
-IMAGE_SHAPE = IMAGE_SIZE + (3,)  # 3 channels for RGB
+IMAGE_SIZE = (64, 64)  # a param to reconsider resolution so different
+IMAGE_SHAPE = IMAGE_SIZE + RGB_CHANNELS  # 3 channels for RGB
 NUM_MASKS = 1  # binary classification (forest vs not forest)
 
 # CNN parameters
@@ -21,5 +22,5 @@ RESOLUTION = ORIGINAL_RESOLUTION * ORIGINAL_IMAGE_SIZE[0]/IMAGE_SIZE[0]
 
 # directories
 DATASET_DIR = "C:\\Users\\jjasi\\datasets\\ForestSegmented"
-TRAINED_MODEL_DIR = f"trained_models/res{int(RESOLUTION)}-lr{LEARNING_RATE}-flt{FILTERS_NUM}-eph{EPOCHS}"
-SENTINEL2_IMAGE_DIR = ""
+TRAINED_MODEL_DIR = f"trained_models/res{RESOLUTION}-lr{LEARNING_RATE}-flt{FILTERS_NUM}-eph{EPOCHS}"
+SENTINEL2_IMAGE_DIR = "./sentinel2-images"
