68625631ca5a1741760b5e396d0de11a3f9edf06
janfiszer <jf.janfiszer@gmail.com>
2023-05-29 00:08:04 +0200

    apply segmanation on the tif images, to clean-up

---
+++ utils/bigImagesOperations.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from utils import visualization
+
+
+def segment(sample_image, model, model_input_size, white_balance=0, display_images=False):
+    input_stride = model_input_size[0]
+
+    if white_balance > 0:
+        sample_image = visualization.white_balancing(sample_image, percentile_value=white_balance)
+
+    full_mask_size_x = int(sample_image.shape[0] * model_input_size[0]/input_stride)
+    full_mask_size_y = int(sample_image.shape[1] * model_input_size[0]/input_stride)
+    full_mask = np.zeros((full_mask_size_x, full_mask_size_y))
+
+    # number of segmentations to perform
+    n_cuts_x = int(full_mask_size_x * model_input_size[0])
+    n_cuts_y = int(full_mask_size_y * model_input_size[0])
+
+    for i in range(n_cuts_x):
+        for j in range(n_cuts_y):
+            patch = sample_image[i*input_stride:(i+1)*input_stride, j * input_stride:(j+1) * input_stride, :]
+
+            image = tf.keras.utils.array_to_img(patch)
+            batch = np.array([tf.image.resize(image, size=model_input_size)])
+
+            if display_images:
+                plt.imshow(image)
+
+            pred_mask = model.predict(batch, verbose=0)
+            
+            output_stride = model_input_size[0]
+            full_mask[i*output_stride:(i+1)*output_stride, j*output_stride:(j+1)*output_stride] = pred_mask.reshape(model_input_size)
+    
+    return full_mask
+
+# TODO: make it work for stride != model_input_size
+# def classify(sample_image, model, model_input_size, input_stride, white_balance=False, display_images=False):
+
+#     # todo followup
+#     input_stride = model_input_size[0]
+
+#     n_cuts = int(sample_image.shape[0]/input_stride)
+    
+#     result_size = int(sample_image.shape[0] * model_input_size[0]/input_stride)
+#     result = np.zeros((result_size, result_size))
+
+#     for i in range(n_cuts):
+#         for j in range(n_cuts):
+#             patch = sample_image[i*input_stride:(i+1)*input_stride, j* input_stride:(j+1) * input_stride, :]
+#             # print(patch.shape)
+#             if white_balance:
+#                 patch = visualization.white_balancing(patch)
+
+#             image = tf.keras.utils.array_to_img(patch)
+#             batch = np.array([tf.image.resize(image, size=model_input_size)])
+
+
+#             if display_images:
+#                 plt.imshow(image)
+
+#             y_pred = model.predict(batch, verbose=0)
+#             predicted_class = np.argmax(y_pred)
+
+#             patch_result = np.ones(model_input_size) * predicted_class
+#             result[i*input_stride:(i+1)*input_stride, j* input_stride:(j+1) * input_stride] = patch_result
+
+
+#     return result
+            
+
